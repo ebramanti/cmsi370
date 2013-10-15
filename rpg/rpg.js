da3e10812f8be9ea05620ec710cc473825b9e706
Edward Bramanti <edward@bramanti.org>
2013-10-15 16:57:50 -0700

    Notes from class & testing out tables

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -8,4 +8,10 @@ $(function () {
         $('#deleteModal').modal('hide');
     });
 
+    $("#confirm-create-button").click(function () {
+        console.log("Create confirmed!!!!!");
+        console.log($('#createModal select').val());
+        //window.location = "character.html#789789789"
+    });
+
 });
