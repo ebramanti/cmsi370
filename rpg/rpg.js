28448161b16ed4ac2f933e84c800c530cc5efce8
Edward Bramanti <edward@bramanti.org>
2013-10-17 04:54:47 -0700

    Testing future bad input check in modal

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -15,14 +15,49 @@ $(function () {
 
     $("#confirm-edit-button").click(function () {
         console.log("Edit confirmed!");
-        window.location = "character.html#" + $()
+        window.location = $("#checkbox input.href").val()
     });
 
-    $('.table table-striped .link').on('click', function(e) {
-	    e.preventDefault();
-	    var $this = $(this);
-	    var $collapse = $this.closest('.collapse-group').find('.collapse');
-	    $collapse.collapse('toggle');
+	/*$("#createModal").validate({
+	    rules: {
+	        name: {
+	            minlength: 1,
+	            required: true
+	        },
+	        race: {
+	            required: true,
+	            !"Select a Race": true
+	        },
+	        gender: {
+	            required: true,
+	            !"Select a Gender": true
+	        },
+	        skinTone: {
+	            required: true,
+	            !"Select a Skin Tone": true
+	        },
+	        weight: {
+	            minlength: 1,
+	            $.isNumeric(): true
+	        }
+	    },
+	    messages: {
+	        name: "Please enter a valid name",
+	        race: "Please select a race",
+	        gender: "Please select a gender",
+	        skinTone: "Please select a skin tone",
+	        weight: "Please enter a valid weight"
+	    },
+	    highlight: function (element, errorClass, validClass) {
+	        $(element).closest('.modal-content').removeClass('success').addClass('error');
+	    },
+	    unhighlight: function (element, errorClass, validClass) {
+	        $(element).closest('.modal-content').removeClass('error').addClass('success');
+	    },
+	    success: function (label) {
+	        $(label).closest('form').find('.valid').removeClass("invalid");
+	    },
+	    errorPlacement: function (error, element) {
+	        element.closest('.modal-content').find('.help-block').html(error.text());
+	    } */
 	});
-
-});
