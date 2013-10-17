b1e4df2b508835cd2b0a3dc067f9973e7e4140fd
Edward Bramanti <edward@bramanti.org>
2013-10-17 01:23:43 -0700

    Changed character names and began implementation of create modal

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -10,8 +10,14 @@ $(function () {
 
     $("#confirm-create-button").click(function () {
         console.log("Create confirmed!!!!!");
-        console.log($('#createModal select').val());
-        //window.location = "character.html#789789789"
+        window.location = "character.html#"
     });
+
+    $('.table table-striped .link').on('click', function(e) {
+	    e.preventDefault();
+	    var $this = $(this);
+	    var $collapse = $this.closest('.collapse-group').find('.collapse');
+	    $collapse.collapse('toggle');
+	});
 
 });
