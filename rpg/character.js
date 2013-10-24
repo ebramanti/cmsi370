8cbd37731a7976a75fdf7b635b37fc2e638bddd2
Edward Bramanti <edward@bramanti.org>
2013-10-24 02:26:40 -0700

    Same as last commit for HTML

--- rpg/character.js
+++ rpg/character.js
@@ -5,6 +5,10 @@ $(function () {
             // Do something with the character.
             console.log(character);
             $("h1 > em > strong").text(character.name);
+            $("#character-table tbody > th:nth-child(1)").text(character.classType);
+            $("#character-table tbody > th:nth-child(2)").text(character.gender);
+            $("#character-table tbody> th:nth-child(3)").text(character.level);
+            $("#character-table tbody> tr> th:nth-child(4)").text(character.money);
         }
     );
 });
