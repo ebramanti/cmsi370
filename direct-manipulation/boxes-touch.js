473ef500c9ff9b583f742495842a9c99d6640d29
Edward Bramanti <edward@bramanti.org>
2013-11-14 03:17:28 -0800

    Box deletion implemented, still having difficulty with creation tracking

--- direct-manipulation/boxes-touch.js
+++ direct-manipulation/boxes-touch.js
@@ -26,7 +26,6 @@ var BoxesTouch = {
             touch.initialX = touch.pageX;
             touch.initialY = touch.pageY;
             var createdBox = '<div class="box" style="width: 0px; height: 0px; left:' + touch.pageX + 'px; top: ' + touch.pageY + 'px">' + '</div>';
-            //var createdBox = divbox;
             $("#drawing-area").append(createdBox);
             touch.creation = $("div div:last-child");
             touch.creation.addClass("creation-highlight");
@@ -47,12 +46,23 @@ var BoxesTouch = {
             // Don't bother if we aren't tracking anything.
             if (touch.target.movingBox) {
                 // Reposition the object.
-                touch.target.movingBox.offset({
+                touch.target.movingBox.offset ({
                     left: touch.pageX - touch.target.deltaX,
                     top: touch.pageY - touch.target.deltaY
                 });
+                //  Adds Deletion Highlight
+                if (!((touch.target.movingBox).hasClass("box-delete")) && (touch.pageX - touch.target.deltaX > 512 || touch.pageY - touch.target.deltaY > 512 || touch.pageX - touch.target.deltaX < 0 || touch.pageY - touch.target.deltaY < 0)) {
+                    touch.target.movingBox.addClass("box-delete deletion-highlight");
+                }
+                
+                /* Removes Deletion Highlight,
+                 * used if() instead of else() due to on-move oscillation between
+                 * box-highlight and deletion-highlight with else() 
+                 */
+                if (((touch.target.movingBox).hasClass("box-delete")) && (touch.pageX - touch.target.deltaX < 512 && touch.pageY - touch.target.deltaY < 512 && touch.pageX - touch.target.deltaX > 0 && touch.pageY - touch.target.deltaY > 0)) {
+                    touch.target.movingBox.removeClass("box-delete deletion-highlight");
+                }
             }
-
             if (touch.creation) {
                 var createLeft, createTop, createWidth, createHeight;
 
@@ -97,7 +107,7 @@ var BoxesTouch = {
      */
     endDrag: function (event) {
         $.each(event.changedTouches, function (index, touch) {
-            if (touch.target.movingBox) {
+            if(touch.target.movingBox) {
                 // Change state to "not-moving-anything" by clearing out
                 // touch.target.movingBox.
                 touch.target.movingBox = null;
@@ -114,7 +124,9 @@ var BoxesTouch = {
      */
     unhighlight: function () {
         $(this).removeClass("box-highlight");
-
+        if ($(this).hasClass("box-delete")) {
+            $(this).remove();
+        }
     },
 
     /**
