7d21e249804bf745cef7fc5538b020fdde5a4ada
Edward Bramanti <edward@bramanti.org>
2013-11-24 21:03:53 -0800

    Formatted block of code correctly

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -122,42 +122,37 @@ $(function () {
         $("#edit-gender").val("Edit Gender");
     });
 
-    //All the code below displays characters in the main table.
-    // JD: This var block (and the statements after) should go out a level---I don't see any
-    //     containing code structure, do you?
-        var characterRowTemplate = '<tr id="">' +
-            '<td><input type="checkbox" class="edit-delete-checkbox" value=""></td>' +
-            '<td><a href=""></a></td>' +
-            '<td></td>' +
-            '<td></td>' +
-            '<td></td>' +
-            '<td></td>' +
-          '</tr>';
-            
-        $.getJSON(
-        "http://lmu-diabolical.appspot.com/characters",
-        function (characters) {
-            characters.forEach(function (character) {
-                var $characterRow = $(characterRowTemplate);
-                /*
-                 * Had difficulty adding id to tr, so left unimplemented.
-                 * Here is a sample of my logic, but it wouldn't work...
-                 *
-                 * $characterRow.find("tr").attr("id", character.id);
-                 */
-                // JD: That didn't work because $characterRow itself is the tr!
-                //     Thus, what you want is $characterRow.attr({ id: character.id });
-                $characterRow.find("td:nth-child(1) > input")
-                    .attr({ value: character.id });
-                $characterRow.find("td:nth-child(2) > a")
-                    .attr({ href: "character.html#" + character.id })
-                    .text(character.name);
-                //For Class Type and Gender, I format the text so that the first letter is capital, and the rest lowercase, regardless of user input.
-                $characterRow.find("td:nth-child(3)").text(character.classType);
-                $characterRow.find("td:nth-child(4)").text(character.gender.substr(0, 1).toUpperCase() + character.gender.substr(1).toLowerCase());
-                $characterRow.find("td:nth-child(5)").text(character.level);
-                $characterRow.find("td:nth-child(6)").text(character.money);
-                $("#character-table > tbody").append($characterRow);
-            });
+    // All the code below displays characters in the main table.
+    // EB: Correctly formatted code structure.
+    var characterRowTemplate = '<tr id="">' +
+        '<td><input type="checkbox" class="edit-delete-checkbox" value=""></td>' +
+        '<td><a href=""></a></td>' +
+        '<td></td>' +
+        '<td></td>' +
+        '<td></td>' +
+        '<td></td>' +
+      '</tr>';
+        
+    $.getJSON(
+    "http://lmu-diabolical.appspot.com/characters",
+    function (characters) {
+        characters.forEach(function (character) {
+            var $characterRow = $(characterRowTemplate);
+            /*
+             * EB: Added id to tr using JD's logic.
+             */
+            $characterRow.attr({ id: character.id });
+            $characterRow.find("td:nth-child(1) > input")
+                .attr({ value: character.id });
+            $characterRow.find("td:nth-child(2) > a")
+                .attr({ href: "character.html#" + character.id })
+                .text(character.name);
+            //For Class Type and Gender, I format the text so that the first letter is capital, and the rest lowercase, regardless of user input.
+            $characterRow.find("td:nth-child(3)").text(character.classType);
+            $characterRow.find("td:nth-child(4)").text(character.gender.substr(0, 1).toUpperCase() + character.gender.substr(1).toLowerCase());
+            $characterRow.find("td:nth-child(5)").text(character.level);
+            $characterRow.find("td:nth-child(6)").text(character.money);
+            $("#character-table > tbody").append($characterRow);
         });
+    });
 });
