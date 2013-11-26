5a65d1c301dc99a180aebc740aa6befda9385542
Edward Bramanti <edward@bramanti.org>
2013-11-26 01:21:38 -0800

    Added possible attributes to widget functionality

--- widget-from-scratch/GenderSwitch.js
+++ widget-from-scratch/GenderSwitch.js
@@ -1,15 +1,27 @@
 //GenderSwitch.js, a JQuery plugin that is a switch for gender in Skyrim Character Creator.
 
 (function ($) {
-    // Private plugin helpers.
     $.widget("gender-switch", {
 
         options: {
+            current_state: undefined,
             state_one: "Male",
             state_two: "Female",
-
             width:,
             height:,
         }
 
-    }
+        _create: function() {
+            //  TODO
+        }
+
+        _init: function() {
+            //  TODO
+        }
+
+        _toggle: function() {
+            //  TODO
+        }
+
+    });
+});
