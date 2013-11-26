b7fbeec93b674051035240e1bbc7c8ee25c25e32
Edward Bramanti <edward@bramanti.org>
2013-11-26 06:20:53 -0800

    Beginning implementation of widget to RPG

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -170,4 +170,10 @@ $(function () {
         });
     });
 
+    $(".character-text-edit").characterLiveEdit ({
+        change: function () {
+            console.log($(this).text()); //Shows in console the changes made.
+        }
+    });
+
 });
