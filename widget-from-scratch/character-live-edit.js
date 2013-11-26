e14ce2ce691ca272421c521f30f1b25ff457104c
Edward Bramanti <edward@bramanti.org>
2013-11-26 02:33:57 -0800

    Stopped using widget factory and changing to implement a live character attribute edit in modal

---
+++ widget-from-scratch/character-live-edit.js
@@ -0,0 +1,7 @@
+//CharacterLiveEdit.js, a JQuery plug-in that will allow live manipulation of given character attributes.
+
+(function ($) {
+    $.fn.characterLiveEdit = function(options) {
+
+    }
+});
