1ca39918759f73726cdbbb6e0f871ecf4d9c6a2d
Edward Bramanti <edward@bramanti.org>
2013-11-26 02:51:22 -0800

    Implementing hover commands, working on way to display word edit next to span and remove on hover end

--- widget-from-scratch/character-live-edit.js
+++ widget-from-scratch/character-live-edit.js
@@ -1,7 +1,18 @@
 //CharacterLiveEdit.js, a JQuery plug-in that will allow live manipulation of given character attributes.
+//Used in-place-editor from dondi/bazaar as a starting point, and making a few edits to work well with what I want for RPG.
 
 (function ($) {
     $.fn.characterLiveEdit = function(options) {
+        var $this = this;
+        $this.addClass("character-editor");
+        .hover(
+            function (event) {
+                $(this).append($("<span></span>").addClass("glyphicon glyphicon-edit"));
+            },
 
+            function (event) {
+                $(this).find("span.glyphicon.glyphicon-edit").remove();
+            }
+        )
     }
-});
+}(jQuery));
