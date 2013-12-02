a04540c9d7111b3e147ecdf48d49db0cc1ee14ad
Edward Bramanti <edward@bramanti.org>
2013-12-01 23:49:45 -0800

    Helper Javascript for modal support

--- widget-from-scratch/demo-code.js
+++ widget-from-scratch/demo-code.js
@@ -4,4 +4,17 @@ $(function () {
             console.log($(this).text()); //Shows in console the changes made.
         }
     });
+
+    //Edits a character.
+    $("#confirm-edit-button").click(function () {
+        $('#editModal').modal('hide');
+    });
+
+    //Clears edit modal if cancelled.
+    $("#editModal").on("hidden.bs.modal", function() {
+        console.log("Clearing stuff son.");
+        $("#edit-name, #edit-level, #edit-money").text("");
+        $("#edit-classType").val("Edit Class Type");
+        $("#edit-gender").val("Edit Gender");
+    });
 });
